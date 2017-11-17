1cb91c29340de7d7059fcf6c4da32f2847ff618a
Harry <yury_regis@hotmail.com>
2017-11-17 20:54:38 -0200

    Original files

---
+++ DataBase.py
@@ -0,0 +1,187 @@
+import sqlite3
+import time
+import datetime
+
+
+def connection_on():
+    connection = sqlite3.connect('banco.db', check_same_thread=False)
+    cursor = connection.cursor()
+    return connection, cursor
+#data = datetime.datetime.fromtimestamp(int(time.time())).strftime('%d/%m %H:%M')
+#print(str(data))
+
+
+def create_table():
+    connection, cursor = connection_on()
+    cursor.execute('CREATE TABLE IF NOT EXISTS dados (data txt, id txt, origem text, destino txt, \
+                    hora text, vagas integer, valor1 real, valor2 real, caroneiros txt)')
+    connection.close()
+
+
+def cancel_carona(passageiro, motorista, data, hora):
+    connection, cursor = connection_on()
+    slc = 'SELECT {} FROM dados where id = ? and data = ? and hora = ?'
+    upd = 'UPDATE dados set {} = ? where id = ? and data = ? and hora = ?'
+    vagas = list(cursor.execute(slc.format('vagas'), (motorista, data, hora)))
+    vagas = vagas[0][0]
+    try:
+        caroneiros = list(cursor.execute(slc.format('caroneiros'), (motorista, data, hora)))
+        caroneiros = [x.lower() for x in caroneiros[0]]
+        caroneiros = caroneiros[0].split()
+        if len(caroneiros) == 0 or passageiro.lower() not in caroneiros:
+            connection.close()
+            return 'Oops! {}, seu nome não consta na lista de caroneiros desta viagem.'.format(passageiro.title())
+        caroneiros.remove(passageiro.lower())
+        caroneiros = [x.title() for x in caroneiros]
+        caroneiros = ' '.join(caroneiros)
+        cursor.execute(upd.format('caroneiros'), (caroneiros, motorista, data, hora))
+        cursor.execute(upd.format('vagas'), (vagas+1, motorista, data, hora))
+        connection.commit()
+    except:
+        connection.close()
+        return 'Oops! Algo de errado não está certo.\nUse /help ou /ajuda para consulta de comandos.'
+    connection.close()
+    return '{} removido(a) do carro de {}.\nViagem: {} às {}'.format(passageiro, motorista, data, hora)
+
+
+def delet_info(nome, data, hora):
+    connection, cursor = connection_on()
+    try:
+        sql = 'DELETE FROM dados WHERE id = ? and data = ? and hora = ?'
+        cursor.execute(sql, (nome, data, hora))
+        connection.commit()
+        connection.close()
+        return 'Viagem excluida ! :)'
+    except:
+        return 'Sinto muito, viagem não localizada. :('
+
+
+def insert_table(data, id, ida, volta, hora, vagas, valor1, valor2, caroneiros=''):
+    connection, cursor = connection_on()
+    cursor.execute('INSERT INTO dados (data, id, origem, destino, hora, vagas, valor1, valor2, caroneiros) '
+                   'VALUES(?,?,?,?,?,?,?,?,?)', (data, id.lower(), ida.lower(), volta.lower(),
+                                                 hora, vagas, valor1, valor2, caroneiros))
+    connection.commit()
+    connection.close()
+
+
+def insert_carona(motorista, data, hora, passageiro):
+    connection, cursor = connection_on()
+    sql = 'UPDATE dados SET {} = ? where id = ? and data = ? and hora = ?'
+    sql2 = 'SELECT {} FROM dados where id = ? and data = ? and hora = ?'
+    vagas = list(cursor.execute(sql2.format('vagas'), (motorista, data, hora)))
+    pas_atual = list(cursor.execute(sql2.format('caroneiros'), (motorista, data, hora)))
+    pas_atual = pas_atual[0][0]
+    vagas = vagas[0][0]
+    if vagas > 0:
+        vagas -= 1
+        pas_atual += ' {}'.format(passageiro)
+        cursor.execute(sql.format('caroneiros'), (pas_atual, motorista, data, hora))
+        cursor.execute(sql.format('vagas'), (vagas, motorista, data, hora))
+    else:
+        connection.close()
+        return 'Infelizmente todas as vagas já foram ocupadas para esta viagem. :('
+    connection.commit()
+    connection.close()
+    return '{} adicionado(a) no caro de {}.\nViagem: {} às {}'.format(passageiro.title(),
+                                                                      motorista.title(), data, hora)
+
+
+def change_table(opcao, valor, origem, destino, data):
+    connection, cursor = connection_on()
+    if opcao.lower() in ['data', 'hora', 'vagas', 'origem', 'destino', 'valor1', 'valor2']:
+        if opcao.lower == 'vagas':
+            valor = int(valor)
+        elif opcao.lower in ['valor1', 'valor2']:
+            valor = float(valor.replace(',', '.'))
+        sql = 'UPDATE dados SET {} = ? where origem = ? and destino = ? and data = ?'.format(opcao.lower())
+        cursor.execute(sql, (valor, origem, destino, data))
+        connection.commit()
+        ans = 'Alterações salvas com sucesso.'
+    else:
+        ans = 'Ooops, algo de errado não esta certo!\nUse o comando /help ou /ajuda para consultar o manual.'
+    connection.close()
+    return ans
+
+
+def format_ans(linha, answer):
+    answer += '\nData: {} Hora: {}'.format(linha[0], linha[4])
+    answer += '\nOrigem: {} Destino: {}'.format(linha[2].capitalize(), linha[3].capitalize())
+    answer += '\nMotorista: {} Vagas: {}'.format(linha[1], linha[5])
+    answer += '\nValor: R${:.2f} ou R${:.2f}'.format(linha[6], linha[7])
+    answer += '\nCaroneiros:'
+    if len(linha[8]) == 0:
+        answer += ' Nenhum'
+    else:
+        for nome in linha[8].split(' '):
+            answer += ' {}'.format(nome)
+    answer += '\n'
+    return answer
+
+
+def search_table(origem, destino, date):
+    connection, cursor = connection_on()
+    sql = 'SELECT * FROM dados WHERE origem = ? and destino = ? and data = ?'
+    answer = '\n'
+    for linha in cursor.execute(sql, (origem.lower(), destino.lower(), date)):
+        answer += format_ans(linha, answer)
+    if len(answer) == 1:
+        answer = '\nNenhuma carona ofertada de {} para {}.'.format(origem.capitalize(), destino.capitalize())
+    connection.close()
+    return answer
+
+
+def simple_search(palavra):
+    connection, cursor = connection_on()
+    answer = '\n'
+    valores = cursor.execute('SELECT * FROM dados;').fetchall()
+    for linha in valores:
+        if palavra.lower() in linha:
+            answer += format_ans(linha, answer)
+    if len(answer) == 1:
+        answer = '\nNenhum resultado para {}'.format(palavra.capitalize())
+    connection.close()
+    return answer
+
+
+def select_date_destiny(origem, destino, data):
+    connection, cursor = connection_on()
+    selecao = tuple(cursor.execute('SELECT * FROM dados where origem = ? and destino = ? and data = ?',
+                             (origem.lower(), destino.lower(), data)))
+    connection.close()
+    return selecao
+
+
+def select_date_id(motorista, data, hora):
+    connection, cursor = connection_on()
+    sql = 'SELECT * FROM dados where id = ? and data = ? and hora = ?'
+    selecao = tuple(cursor.execute(sql, (motorista, data, hora)))
+    connection.close()
+    return selecao
+
+
+def clean_table(date):
+    connection, cursor = connection_on()
+    try:
+        cursor.execute('DELETE FROM dados WHERE data < ?', (date,))
+        connection.commit()
+        connection.close()
+        return 'Limpeza do banco de dados concluida ! :)'
+    except:
+        return 'Sinto muito, não foi possível completar a limpeza do banco de dados. :('
+
+
+def check_value(value, date):
+    connection, cursor = connection_on()
+    for linha in cursor.execute('SELECT * FROM dados;').fetchall():
+        if value in linha and date in linha:
+            connection.close()
+            return True
+        else:
+            pass
+    connection.close()
+    return False
+
+
+ans = clean_table('17/11')
+print(ans)
---
+++ main.py
@@ -0,0 +1,160 @@
+import telebot
+import DataBase
+import datetime
+
+
+bot = telebot.TeleBot('501268361:AAETOUTYahtlvhGR0MzTw0AeE74OZ1Hdac8')
+msg_error = 'Oops! Algo de errado não está certo.\nUse /help ou /ajuda para consulta de comandos.'
+
+
+@bot.message_handler(commands=['ajuda', 'help'], content_types='text')
+def new_message(message):
+    arquivo, txt = open('help_command.txt','r'), ''
+    for linha in arquivo.readlines(): txt += linha
+    bot.send_message(message.chat.id, txt)
+
+
+@bot.message_handler(commands='start', content_types='text')
+def new_message(message):
+    name = message.from_user.first_name
+    txt = 'Bem-vindo(a) {} ! :)\nDigite /ajuda ou /help para me chamar.'
+    bot.send_message(message.chat.id, txt.format(name))
+
+
+@bot.message_handler(commands='carona', content_types='text')
+def new_message(message):
+    name = message.from_user.first_name.split()
+    name = [x.lower() for x in name]
+    name = '_'.join(name)
+    texto = (message.text.split(maxsplit=1)[1].split())
+    texto = [texto[x].lower() for x in range(len(texto))]
+    try:
+        if len(texto) == 2:
+            date = datetime.datetime.fromtimestamp(message.date).strftime('%m/%d')
+            motorista, hora = texto[0], texto[1]
+        else:
+            date, motorista, hora = texto[0], texto[1], texto[2]
+        if DataBase.check_value(motorista, date):
+            ans = DataBase.insert_carona(motorista, date, hora, name)
+            bot.message_handler(message.chat.id, ans)
+        else:
+            bot.message_handler(message.chat.id, 'Não localizamos oferta de {} para {}. :('.format(motorista, date))
+    except:
+        bot.message_handler(message.chat.id, msg_error)
+
+
+@bot.message_handler(commands='cancela', content_types='text')
+def new_message(message):
+    name = message.from_user.first_name.split()
+    name = [x.lower() for x in name]
+    name = '_'.join(name)
+    texto = message.text.split(maxsplit=1)[1].split()
+    texto = [x.lower() for x in texto]
+    try:
+        if len(texto) == 2:
+            motorista, hora = texto[0], texto[1]
+            date = datetime.datetime.fromtimestamp(message.date).strftime('%d/%m')
+            ans = DataBase.cancel_carona(name, motorista, date, hora)
+        else:
+            motorista, date, hora = texto[0], texto[1], texto[2]
+            ans = DataBase.cancel_carona(name, motorista, date, hora)
+        bot.send_message(message.chat.id, ans)
+    except:
+        bot.send_message(message.chat.id, msg_error)
+
+
+@bot.message_handler(commands='excluir', content_types='text')
+def new_message(message):
+    name = message.from_user.first_name.split()
+    name = [x.lower() for x in name]
+    name = '_'.join(name)
+    try:
+        texto = message.text.split(maxsplit=1)[1].split()
+        if len(texto) == 1:
+            date = datetime.datetime.fromtimestamp(message.date).strftime('%d/%m')
+            hora = texto[0]
+        else:
+            date, hora = texto[0], texto[1]
+        ans = DataBase.delet_info(name, date, hora)
+        bot.send_message(message.chat.id, ans)
+    except:
+        bot.send_message(message.chat.id, msg_error)
+
+
+@bot.message_handler(commands='ofertar', content_types='text')
+def new_message(message):
+    name = message.from_user.first_name.split()
+    name = [x.lower() for x in name]
+    name = '_'.join(name)
+    try:
+        texto = (message.text.split(maxsplit=1)[1].split())
+        texto = [x.lower() for x in texto]
+        if len(texto) == 7:
+            texto[4], texto[5], texto[6] = int(texto[4]), float(texto[5].replace(',','.')), \
+                                           float(texto[6].replace(',','.'))
+            date, ida, volta, hour, vagas, v1, v2 = tuple(texto)
+        else:
+            texto[3], texto[4], texto[5] = int(texto[3]), float(texto[4].replace(',', '.')), \
+                                           float(texto[5].replace(',', '.'))
+            date = datetime.datetime.fromtimestamp(message.date).strftime('%d/%m')
+            ida, volta, hour, vagas, v1, v2 = tuple(texto)
+        DataBase.insert_table(date, name, ida, volta, hour, vagas, v1, v2)
+        bot.send_message(message.chat.id, '{}, sua oferta foi adicionada.'.format(name.title()))
+    except:
+        bot.send_message(message.chat.id, msg_error)
+
+
+@bot.message_handler(commands='buscar', content_types='text')
+def new_message(message):
+    name = message.from_user.first_name.split()
+    name = [x.lower() for x in name]
+    name = '_'.join(name)
+    try:
+        texto = tuple(message.text.split(maxsplit=1)[1].split())
+        date = datetime.datetime.fromtimestamp(message.date).strftime('%d/%m')
+        if len(texto) == 2:
+            ida, volta = texto
+            ans = DataBase.search_table(ida, volta, date)
+        else:
+            palavra = texto
+            ans = DataBase.simple_search(palavra)
+        bot.send_message(message.chat.id, '{}, aqui esta o resultado da sua busca:'.format(name.title()) + ans)
+    except:
+        bot.send_message(message.chat.id, msg_error)
+
+
+@bot.message_handler(commands='alterar', content_types='text')
+def new_message(message):
+    name = message.from_user.first_name.split()
+    name = [x.lower() for x in name]
+    name = '_'.join(name)
+    try:
+        texto = tuple(message.text.split(maxsplit=1)[1].split())
+        date = datetime.datetime.fromtimestamp(message.date).strftime('%d/%m')
+        if DataBase.check_value(name, date):
+            if len(texto) == 5:
+                origem, destino, date, opcao, valor = texto[0], texto[1], texto[2], texto[3], texto[4]
+                selecao = DataBase.select_date_destiny(origem, destino, date)
+            else:
+                origem, destino, opcao, valor = texto[0], texto[1], texto[2], texto[3]
+                selecao = DataBase.select_date_destiny(origem, destino, date)
+            ans = DataBase.change_table(opcao, valor, selecao[0][2], selecao[0][3], selecao[0][0])
+            bot.send_message(message.chat.id, ans)
+        else:
+            ans = '{}, somente o motorista pode alterar/excluir viagem'.format(name.title())
+            bot.send_message(message.chat.id, ans)
+    except:
+        bot.send_message(message.chat.id, msg_error)
+
+
+@bot.message_handler(commands='limpar', content_types='text')
+def new_message(message):
+    date = datetime.datetime.fromtimestamp(message.date).strftime('%d/%m')
+    ans = DataBase.clean_table(date)
+    bot.send_message(message.chat.id, ans)
+
+
+print("executando...")
+DataBase.create_table()
+
+bot.polling(none_stop=False, timeout=20, interval=0)

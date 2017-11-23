dfa74915cfbd31e1066ceb939f7cdfbf5bf52a1e
Harry <yury_regis@hotmail.com>
2017-11-23 17:34:50 -0200

    update main-1.0.4
    add registro automatico de login

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import traceback
 import markup
 import time
 
-
-bot = telebot.TeleBot('501268361:AAHp056OeZVAC3oE2dQtjMKyDucPHfz0Ya0')
+token = '' #  <Token geradp pelo telegram via @BotFather>
+bot = telebot.TeleBot(token)
 msg_error = 'Oops! Algo de errado não está certo.\nUse /help ou /ajuda para consulta de comandos.'
 removemkp = telebot.types.ReplyKeyboardHide()
 global group_id
-group_id = -1001379699085
+group_id = ''  #  <id do grupo/supergrupo que o bot irá administrar>
 global adm_id
-adm_id = [473906011]
+adm_id = [] #  <Lista de id's dos administradores do bot/grupo/supergrupo>
 
 
 @bot.message_handler(commands=['ajuda', 'help'], content_types='text')
